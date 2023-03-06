e53b576d7dd21f46e1f40a5126778a1577088064
ViktorRomanovsky <Romanovsky0815@gmail.com>
2023-03-06 20:27:58 +0300

    init: welcome

--- stage1-tasks-momentum/script.js
+++ stage1-tasks-momentum/script.js
@@ -1,19 +1,34 @@
 const time = document.querySelector('.time');
 const dates = document.querySelector('.date');
+
 function showDate() {
     const date = new Date();
 
     const options = {
         month: 'long',
         day: 'numeric',
-        hour: 'numeric',
-
+        weekday: 'long',
         timeZone: 'UTC',
     };
     const currentDate = date.toLocaleDateString('en-En', options);
     dates.textContent = currentDate;
 }
-showDate();
+const greeting = document.querySelector('.greeting');
+
+function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+
+    if (6 <= hours && hours < 12) {
+        return (greeting.textContent = 'Good morning, ');
+    } else if (12 <= hours && hours < 18) {
+        return (greeting.textContent = 'Good afternoon, ');
+    } else if (18 <= hours && hours > 0) {
+        return (greeting.textContent = 'Good evening, ');
+    } else {
+        return (greeting.textContent = 'Good night, ');
+    }
+}
 
 function showTime() {
     const date = new Date();
@@ -21,5 +36,21 @@ function showTime() {
     time.textContent = currentTime;
     setTimeout(showTime, 1000);
     showDate();
+    getTimeOfDay();
 }
 showTime();
+
+const names = document.querySelector('.name');
+
+function setLocalStorage() {
+    localStorage.setItem('name', names.value);
+}
+
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+    if (localStorage.getItem('name')) {
+        names.value = localStorage.getItem('name');
+    }
+}
+window.addEventListener('load', getLocalStorage);
